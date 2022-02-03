804ad88eabd09055179b1ed474d2a179b3e8e82b
anj180190 <anj180190@gmail.com>
2022-02-03 15:37:08 +0530

    Initial setup

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports={
+    entry:"./src/index.js",
+    mode:"development",
+    output:{
+        path:path.resolve(__dirname,"dist"),
+        filename:"bundle.js"
+    },
+    module:{
+        rules:[
+            {
+                test:/\.?js$/,
+                exclude:/node_module/,
+                use:{
+                    loader:"babel-loader",
+                    options:{
+                        presets:['@babel/preset-env','@babel/preset-react']
+                    }
+                }
+            }
+        ]
+    },
+    plugins:[
+        new HtmlWebpackPlugin({template:path.join(__dirname,"public","index.html")})
+    ]
+}

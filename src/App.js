94708f8167f043ba4cea8a422fa0d1787adae10f
anj180190 <anj180190@gmail.com>
2022-03-06 02:14:02 +0530

    new

--- src/App.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const App =()=>(
-    <h1>Hello World AJ .....</h1>
-);
-
-export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,26 @@
-const path = require("path");
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports={
-    entry:"./src/index.js",
-    mode:"development",
-    output:{
-        path:path.resolve(__dirname,"dist"),
-        filename:"bundle.js"
-    },
-    module:{
-        rules:[
-            {
-                test:/\.?js$/,
-                exclude:/node_module/,
-                use:{
-                    loader:"babel-loader",
-                    options:{
-                        presets:['@babel/preset-env','@babel/preset-react']
-                    }
-                }
-            }
-        ]
-    },
-    plugins:[
-        new HtmlWebpackPlugin({template:path.join(__dirname,"public","index.html")})
-    ]
-}
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.?js$/,
+        exclude: /node_module/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: path.join(__dirname, 'public', 'index.html') })],
+};
